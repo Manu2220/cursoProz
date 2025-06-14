551c16d8766e85c2c0a1e22e12c2c930f6a89833
Manu2220 <nanuzinhasouza@gmail.com>
2025-06-14 12:17:04 -0300

    subindo aula 07

--- script.js
+++ script.js
@@ -3,20 +3,41 @@ function mostrarMensagem() {
         }
 
         function verificarNumero() {
-            let nomero = document.getElementById(número).value;
+            let numero = document.getElementById(numero).value;
             numero = Number(numero);
             let mensagem = "";
 
-            if (numero >0) {
+            if (numero > 0) {
                 mensagem = "0 número é positivo.";
-            }
-            else if (numero < 0) {
+            } else if (numero < 0) {
                 mensagem = "o número é negativo.";
-            }
-            else {
+            } else {
                 mensagem = "o número é zero.";
             }
 
-            document.getElementById("esultado").innerText = mensagem;
+            document.getElementById("resultado").innerText = mensagem;
             
         }
+
+function executarFor() {
+    let resultado = "Número pares de 0 a 10:";
+    for (let i = 0; i <= 10; i++) {
+      if(i % 2 === 0) {
+        resultado += i + " ";
+    
+    } 
+}
+    document.getElementById("saida").textContent = resultado;
+}
+
+function executarArray() {
+    let nomes = ["Manu", "Samira", "Andreia", "Ana"];
+    let resultado ="Lista de Alunas";
+
+    for (let i = 0; i < nomes.length; i++) {
+        resultado += nomes[i]
+    }
+    document.getElementById("saida").textContent = resultado;
+
+}
+
